961bce2d0c61d15d97e731767a1c54d1f122d6a2
cyberfolk <andrea1995ferrari@gmail.com>
2024-09-03 01:51:37 +0200

    [UPD] center zoom macroview + test hex-tiles-img

--- cf_hex_base/static/src/view_macro/view_macro.js
+++ cf_hex_base/static/src/view_macro/view_macro.js
@@ -13,30 +13,16 @@ class ViewMacro extends Component {
         super.setup();
         this.orm = useService("orm");
         this.macro = null
-        this.zoom = '00'
+        this.zoom = '100%'
 
         onWillStart(async () => {
             this.macro = await this.orm.call("hex.macro", "get_json_macro", [], {})
             .then((result) => { return JSON.parse(result) })
         })
     }
-    setZoom00(){
-        this.zoom = '00'
-        console.log(this.zoom)
-            this.render();
-
-    }
-    setZoom50(){
-        this.zoom = '50'
-        console.log(this.zoom)
-            this.render();
-
-    }
-    setZoom75(){
-        this.zoom = '75'
-        console.log(this.zoom)
-            this.render();
-
+    setZoom(percentage){
+        this.zoom = percentage
+        this.render();
     }
 
     getHexStyle(hex) {
